e44ea4290628d96cbe446eed976e5eeae81e08e3
zhengmengyu <zhengmengyu987@163.com>
2023-07-14 15:46:54 +0800

    init

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-<!--
- * @Author: zhengmengyu zhengmengyu987@163.com
- * @Date: 2023-07-12 21:22:24
- * @LastEditors: zhengmengyu zhengmengyu987@163.com
- * @LastEditTime: 2023-07-12 21:22:46
- * @FilePath: \algorithm\README.md
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-算法练习仓库
+# 算法练习
--- day1.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: zhengmengyu zhengmengyu987@163.com
- * @Date: 2023-07-12 21:06:45
- * @LastEditors: zhengmengyu zhengmengyu987@163.com
- * @LastEditTime: 2023-07-12 21:07:25
- * @FilePath: \algorithm\day1.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-console.log("hello world!");
